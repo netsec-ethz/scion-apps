1c6c3caf84890f96cdbdef0974daf342023a28af
chaehni <chaehni@student.ethz.ch>
2018-10-14 23:26:18 +0200

    server update

--- server/http/server.go
+++ server/http/server.go
@@ -1,13 +1,15 @@
 package shttp
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net"
+	"strings"
 
+	"github.com/chaehni/scion-http/utils/utils"
 	"github.com/scionproto/scion/go/lib/snet"
 	"github.com/scionproto/scion/go/lib/snet/squic"
-	"gihtub.com/chaehni/scion-http/utils/utils"
 )
 
 type Server struct {
@@ -27,26 +29,44 @@ func (srv *Server) ListenAndServe() {
 	}
 
 	for {
-		c, err := li.Accept()
-		defer c.Close()
+		// wait for new sessions
+		sess, err := li.Accept()
+		defer sess.Close()
 
 		if err != nil {
 			log.Printf(err.Error())
+			continue
 		}
 
-		bs, err := ioutil.ReadAll(c)
+		// get stream of session
+		stream, err := sess.AcceptStream()
 		if err != nil {
-			log.Printf(err.Error())
+			log.Printf("Failed to accept incoming stream (%s)", err)
+			continue
 		}
-		fmt.Println(string(bs))
+
+		// Handle connection
+		go handle(stream)
 	}
+}
+
+// TODO: create dummy type that wraps session and adds missing methods to make it a net.Conn
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	request(conn)
+
+	// respond to reques
+	respond(conn)
+
 }
 
 func (srv *Server) initSCIONConnection(serverAddress, tlsCertFile, tlsKeyFile string) (*snet.Addr, error) {
 
 	log.Println("Initializing SCION connection")
 
-	Srv.Addr, err := snet.AddrFromString(srv.AddrString)
+	Srv.Addr, err = snet.AddrFromString(srv.AddrString)
 	if err != nil {
 		return nil, err
 	}
@@ -58,9 +78,40 @@ func (srv *Server) initSCIONConnection(serverAddress, tlsCertFile, tlsKeyFile st
 
 	err = squic.Init(tlsKeyFile, tlsCertFile)
 	if err != nil {
-		return serverCCAddr, err
+		return Srv.Addr, err
 	}
 
-	return serverCCAddr, nil
+	return Srv.Addr, nil
 
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// request line
+			m := strings.Fields(ln)[0]
+			fmt.Println("***METHOD", m)
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+		i++
+	}
+}
+
+func respond(conn net.Conn){
+	body := `<!DOCTYPE html><html lang="en"><head><meta
+		charset="UTF-8"><title>sample server</title></head>
+		<body><strong>Hello World></strong></body></html>`
+	
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
