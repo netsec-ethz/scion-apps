2cbf213834a2e090763f1019939aae086f2f6ba3
Claude Hähni <chaehni@student.ethz.ch>
2018-11-02 13:57:24 +0100

    made path selection a utility function

--- examples/minimal/client.go
+++ examples/minimal/client.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/chaehni/scion-http/http"
 	"github.com/chaehni/scion-http/utils"
-	"github.com/scionproto/scion/go/lib/sciond"
 	"github.com/scionproto/scion/go/lib/snet"
-	"github.com/scionproto/scion/go/lib/spath"
-	"github.com/scionproto/scion/go/lib/spath/spathmeta"
 )
 
 func main() {
@@ -34,18 +28,7 @@ func main() {
 	dispatcherPath := utils.GetDispatcher()
 
 	if *interactive {
-		err := snet.Init(lAddr.IA, sciondPath, dispatcherPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pathEntry := choosePath(lAddr, rAddr)
-		if pathEntry == nil {
-			log.Fatal("No paths available to remote destination")
-		}
-		rAddr.Path = spath.New(pathEntry.Path.FwdPath)
-		rAddr.Path.InitOffsets()
-		rAddr.NextHopHost = pathEntry.HostInfo.Host()
-		rAddr.NextHopPort = pathEntry.HostInfo.Port
+		utils.ChoosePath(lAddr, rAddr, sciondPath, dispatcherPath) 
 	}
 
 	// Make a map from URL to *snet.Addr
@@ -106,41 +89,4 @@ func printResponse(resp *http.Response, hasBody bool) {
 	}
 	log.Println("Body: ", string(body))
 	fmt.Println("\n\n")
-}
-
-func choosePath(local *snet.Addr, remote *snet.Addr) *sciond.PathReplyEntry {
-
-	pathMgr := snet.DefNetwork.PathResolver()
-	pathSet := pathMgr.Query(local.IA, remote.IA)
-	var appPaths []*spathmeta.AppPath
-	var selectedPath *spathmeta.AppPath
-
-	if len(pathSet) == 0 {
-		return nil
-	}
-
-	fmt.Printf("Available paths to %v\n", remote.IA)
-	i := 0
-	for _, path := range pathSet {
-		appPaths = append(appPaths, path)
-		fmt.Printf("[%2d] %s\n", i, path.Entry.Path.String())
-		i++
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Printf("Choose path: ")
-		scanner.Scan()
-		pathIndexStr := scanner.Text()
-		pathIndex, err := strconv.Atoi(pathIndexStr)
-		if err == nil && 0 <= pathIndex && pathIndex < len(appPaths) {
-			selectedPath = appPaths[pathIndex]
-			break
-		}
-		fmt.Printf("Error: Invalid path index %v, valid indices range: [0,  %v]\n", pathIndex, len(appPaths)-1)
-	}
-
-	entry := selectedPath.Entry
-	fmt.Printf("Using path:\n %s\n", entry.Path.String())
-	return entry
 }
