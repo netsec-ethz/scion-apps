ca08d16c57adbf8fb98c84d26a6f89e9c83a8bdd
Juan A. Garcia Pardo <juagargi@gmail.com>
2019-02-01 15:39:47 +0100

    Adapt to new topology dictionary

--- webapp/tests/health/testVPN.sh
+++ webapp/tests/health/testVPN.sh
@@ -24,23 +24,18 @@ then
     error_exit "You are probably behind a firewall that does not allow UDP traffic on port 1194. Please check your /var/log/syslog to find out if there had been a timeout while trying to establish the openvpn connection (search for ovpn-client in the /var/log/syslog file). If you find out that the tun0 interface was not brought up because timeouts between your client and the VPN server, it is an indication that a firewall is filtering the traffic: please contact your IT service to add an exception for your machine and port 1194."
 fi
 
-
 # ip address of the tun0 interface
 ipAddress=$(echo "$targetLines" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n1)
-
 topologyFile=$SC/gen/ISD*/AS*/endhost/topology.json
-# find the ISD_AS for this VM
-isd_as=$(cat $topologyFile | python -c "import sys, json; print json.load(sys.stdin)['ISD_AS'];" | tr ":" "_")
+# ip address specified in the topology file. If "bind" parameter present, use that one; "public" if not
+ipTopology=$(cat $topologyFile | python -c "import sys, json
+brs = json.load(sys.stdin)['BorderRouters']
+interfaces=next(iter(brs.values()))['Interfaces']
+inter=next(iter(interfaces.values()))
+print inter['BindOverlay'] if 'BindOverlay' in inter.keys() else inter['PublicOverlay']['Addr']")
 
-# decode the ip adresses of "Bind" and "Public" in the above block
-ipBind=$(cat $topologyFile | python -c "import sys, json; print json.load(sys.stdin)['BorderRouters']['br$isd_as-1']['Interfaces']['1']['Bind']['Addr'];")
-
-ipPublic=$(cat $topologyFile | python -c "import sys, json; print json.load(sys.stdin)['BorderRouters']['br$isd_as-1']['Interfaces']['1']['Public']['Addr'];")
-
-# 2.check if the ip address from the tun0 interface is consistent with the two ip addresses above.
-
-if [[ $ipAddress != $ipBind || $ipAddress != $ipPublic ]]
-then
+# 2.check if the ip address from the tun0 interface is consistent with the one from the topology.
+if [[ $ipAddress != $ipTopology ]]; then
     error_exit "The tun0 IP address doesn't match the IP address in your topology file, please destroy the existing virtual machine and remove its settings by first logging out of it and then running the steps described in the snippet vagrant destroy. After destroying the virtual machine, we can delete its configuration:
 
 $ vagrant destroy -f
@@ -54,5 +49,5 @@ $ rm -r user@example.com_17-ffaa_1_64
 Now check in the Coordinator webpage that your AS is correctly attached to your AP of choice, and that you are using the right tarball file. If in doubt, you can always click on Re-download my SCIONLab AS Configuration to get it again. Re-download does not configure the AS, but returns the latest configuration the Coordinator has for it. Wait 15 minutes (the reason being sometimes the attachment point needs 15 minutes to process your request). You should have received an email stating the success of your request. In the hopefully successful state, start again from the checking tarbal step. If after waiting these 15 minutes you did not receive the success email, or you received it but still don't see the same IP address in the tun0 interface as in the topology file, contact us."
 fi
 
-echo "VPN tun0 interface found at $ipAddress matching public and binding addresses for AS $isd_as"
+echo "VPN tun0 interface found at $ipAddress matching binding or public address"
 echo "Test for VPN succeeds."
