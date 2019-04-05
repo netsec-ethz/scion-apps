3798ae3683131182ce94c481b8e441331fa8ffcb
Michael Farb <mwfarb@andrew.cmu.edu>
2019-04-05 13:32:53 -0400

    cleanup go code missed from last cleanup

--- webapp/models/path/db.go
+++ webapp/models/path/db.go
@@ -1,27 +1,27 @@
 package pathdb
 
 import (
-    "database/sql"
+	"database/sql"
 
-    . "github.com/netsec-ethz/scion-apps/webapp/util"
+	. "github.com/netsec-ethz/scion-apps/webapp/util"
 )
 
 func InitDB(filepath string) *sql.DB {
-    var err error
-    db, err := sql.Open("sqlite3", filepath+"?mode=ro")
-    if CheckError(err) {
-        panic(err)
-    }
-    err = db.Ping()
-    if CheckError(err) {
-        panic(err)
-    }
-    return db
+	var err error
+	db, err := sql.Open("sqlite3", filepath+"?mode=ro")
+	if CheckError(err) {
+		panic(err)
+	}
+	err = db.Ping()
+	if CheckError(err) {
+		panic(err)
+	}
+	return db
 }
 
 func CloseDB(db *sql.DB) {
-    err := db.Close()
-    if CheckError(err) {
-        panic(err)
-    }
+	err := db.Close()
+	if CheckError(err) {
+		panic(err)
+	}
 }
