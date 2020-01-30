7f195c138970904ac5528dc10a79d0b6926feec5
Matthias Frei <matthias.frei@inf.ethz.ch>
2020-01-30 23:50:37 +0100

    Simplify and extend hostname resolution
    Make the hostname resolution more "pluggable", using separate Resolvers
    for reading from /etc/hosts and RAINS.
    
    Resolving from the /etc/hosts file (by hostsfileResolver) now reads the
    file on every request instead of parsing on startup. This is more
    aligned with what e.g. gethostbyname does, making modifications to the
    file visible to running applications.
    
    Because it's easy, add an additional resolver that loads
    /etc/scion/hosts, in the same format as /etc/hosts. The idea is that by
    having a separate file, we can now easily distribute a simple hosts file
    from the coordinator.
    
    Remove the fixed hosts table with `AddHost` and the only
    half-implemented reverse lookup `GetHostnamesByAddress`.
    
    Add explicit build tag 'rains' to build with RAINS support.
    Building with `make 'TAGS=!rains'` (or just empty tags) will build
    without rains.
    This is handy e.g. when updating the SCION dependency with API breaking
    changes here in scion-apps but RAINS has not yet been updated.

--- pkg/shttp/transport_test.go
+++ pkg/shttp/transport_test.go
@@ -26,6 +26,8 @@ import (
 
 	"github.com/lucas-clemente/quic-go"
 	"github.com/netsec-ethz/scion-apps/pkg/appnet"
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/snet"
 )
 
 func TestMangleSCIONAddrURL(t *testing.T) {
@@ -70,11 +72,28 @@ func TestMangleSCIONAddrURL(t *testing.T) {
 	}
 }
 
+type mockResolver struct {
+	table map[string]string
+}
+
+func (r *mockResolver) Resolve(name string) (*snet.SCIONAddress, error) {
+	address, ok := r.table[name]
+	if !ok {
+		return nil, &appnet.HostNotFoundError{Host: name}
+	} else {
+		a, err := snet.ParseUDPAddr(address)
+		if err != nil {
+			panic(fmt.Sprintf("test input must parse %s", err))
+		}
+		return &snet.SCIONAddress{IA: a.IA, Host: addr.HostFromIP(a.Host.IP)}, nil
+	}
+}
+
 func TestRoundTripper(t *testing.T) {
 
 	// host2 from hosts_test_file
 	// XXX we should reorganize this hostname resolution to allow configuring the host_tests_file
-	_ = appnet.AddHost("host2", "17-ffaa:0:1,[192.168.1.1]")
+	resolver := &mockResolver{map[string]string{"host2": "17-ffaa:0:1,[192.168.1.1]"}}
 
 	testCases := []struct {
 		HostPort string
@@ -97,7 +116,7 @@ func TestRoundTripper(t *testing.T) {
 	var expected string
 	testDial := func(network, address string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlySession, error) {
 		unmangled := appnet.UnmangleSCIONAddr(address)
-		resolvedAddr, err := appnet.ResolveUDPAddr(unmangled)
+		resolvedAddr, err := appnet.ResolveUDPAddrAt(unmangled, resolver)
 		if err != nil {
 			t.Fatalf("unexpected error when resolving address '%s' in roundtripper: %s", unmangled, err)
 		}
