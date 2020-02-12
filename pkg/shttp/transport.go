c38e2d0ac1f124a51c80f9ed5d4976acd459af90
Matthias Frei <matthias.frei@inf.ethz.ch>
2020-02-12 10:16:28 +0100

    Cleanup license boilerplates
    Remove mysterious copy-pasted "package main" string, add missing
    boilerplate to some files.

--- pkg/shttp/transport.go
+++ pkg/shttp/transport.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.package main
+// limitations under the License.
 
 // This is the low-level Transport implementation of the RoundTripper interface for use with SCION/QUIC
 // The high-level interface is in http/client.go
--- webapp/webapp.go
+++ webapp/webapp.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.package main
+// limitations under the License.
 
 // NOTE: Webapp relies on SCION's configuration for some of its functionality.
 // If the topology changes, webapp should be restarted as well.
